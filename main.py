d4c2c73e3cecf82f18ffeacee3ba13a768c39d61
Szymon Wysocki <219553085+szwys02@users.noreply.github.com>
2026-05-15 20:26:02 +0200

    Create initial pseudocode

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import packages
+import PySimpleGUI as gs
+import pandas as pd
+import geopandas as gpd
+
+# Download data from geoportal
+
+# Create GUI with user's input
+    # Ask user for administrative border
+    # Choosing specific boundaries by name is optional
+
+# Manage downloaded data
+    # Management must be sensible to user's input
