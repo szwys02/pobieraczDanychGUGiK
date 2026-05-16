fb155188da613aae4e0e5192a90ca1cd2e981316
Szymon Wysocki <219553085+szwys02@users.noreply.github.com>
2026-05-16 15:01:41 +0200

    Test zipfile module

--- main.py
+++ main.py
@@ -2,8 +2,20 @@
 import PySimpleGUI as gs
 import pandas as pd
 import geopandas as gpd
+import requests
+import zipfile
+import io
 
 # Download data from geoportal
+# Create URL for every administrative border, WMS link: https://integracja.gugik.gov.pl/cgi-bin/PanstwowyRejestrGranic
+# url = 'https://integracja.gugik.gov.pl/PRG/pobierz.php?jednostki_administracyjne_shp' 
+
+print("Trwa pobieranie danych")
+response = requests.get(url, stream = True)
+zipf = zipfile.ZipFile(io.BytesIO(response.content))
+
+zipf.extractall()
+print("Wykononano zadanie")
 
 # Create GUI with user's input
     # Ask user for administrative border
