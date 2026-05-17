67f71a58215a31a9f72849c722a8fc4b305843b6
Szymon Wysocki <219553085+szwys02@users.noreply.github.com>
2026-05-17 15:35:42 +0200

    Change logic of main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-# Import packages
-import PySimpleGUI as gs
-import pandas as pd
+from requests import Request
 import geopandas as gpd
-import requests
-import zipfile
-import io
 
 # Download data from geoportal
-# Create URL for every administrative border, WMS link: https://integracja.gugik.gov.pl/cgi-bin/PanstwowyRejestrGranic
-# url = 'https://integracja.gugik.gov.pl/PRG/pobierz.php?jednostki_administracyjne_shp' 
+url = 'http://mapy.geoportal.gov.pl/wss/service/PZGIK/PRG/WFS/AdministrativeBoundaries?language=pol&SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES='
+url_rest = '&STARTINDEX=0&COUNT=999999&SRSNAME=urn:ogc:def:crs:EPSG::2180'
+layer = 'ms:A02_Granice_powiatow'
+full_url = url + layer + url_rest
 
-print("Trwa pobieranie danych")
-response = requests.get(url, stream = True)
-zipf = zipfile.ZipFile(io.BytesIO(response.content))
-
-zipf.extractall()
-print("Wykononano zadanie")
+wfs_get = Request('GET', full_url).prepare().url
 
 # Create GUI with user's input
     # Ask user for administrative border
     # Choosing specific boundaries by name is optional
 
 # Manage downloaded data
-    # Management must be sensible to user's input
+# Management must be sensible to user's input
+gdf = gpd.read_file(wfs_get)
+gdf_subset = gdf[gdf['JPT_KOD_JE'].str.startswith('25')]
+gdf.to_file('~/gis/test.gpkg', driver='GPKG')
+gdf_subset.to_file('~/gis/test_sub.gpkg', driver='GPKG')
