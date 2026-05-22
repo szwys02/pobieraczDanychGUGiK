ed92bacc70f4cd7baf13421354bad02102399376
Szymon Wysocki <219553085+szwys02@users.noreply.github.com>
2026-05-22 09:56:20 +0200

    Change module to Qt6

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 # Import external modules
-import PySimpleGUI as sg
+import PySide6
 # Create GUI with user's input
     # Ask user for administrative border
     # Choosing specific boundaries by name is optional
